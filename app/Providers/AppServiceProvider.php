b4ad0816f3ef44ab354727e5a112c69e5e8ed3f2
Mateusz <tylkomateusz@gmail.com>
2017-05-07 15:56:16 +0200

    maria db fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+ 
     /**
      * Bootstrap any application services.
      *
@@ -13,8 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
+
 
     /**
      * Register any application services.
